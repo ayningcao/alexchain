f6789e653c5b381d7932b808efd1bd98f620199e
ayning <1012706684@qq.com>
2018-02-28 22:37:21 +0800

    SHA256的Java实现

---
+++ src/main/java/com/ayning/alexchain/util/DigestUtil.java
@@ -0,0 +1,46 @@
+package com.ayning.alexchain.util;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * created by 10127 on 2018/2/28
+ */
+public class DigestUtil {
+
+    public static String sha256(String str) {
+        MessageDigest messageDigest;
+        String encodeStr = "";
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.update(str.getBytes("UTF-8"));
+            encodeStr = byte2Hex(messageDigest.digest());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return encodeStr;
+    }
+
+    /**
+     * 将byte转为16进制
+     *
+     * @param bytes
+     * @return
+     */
+    private static String byte2Hex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        String temp;
+        for (byte aByte : bytes) {
+            temp = Integer.toHexString(aByte & 0xFF);
+            if (temp.length() == 1) {
+                //1得到一位的进行补0操作
+                sb.append("0");
+            }
+            sb.append(temp);
+        }
+        return sb.toString();
+    }
+}
--- src/test/java/learn/Sha256.java
+++ src/test/java/learn/Sha256.java
@@ -1,9 +1,21 @@
 package learn;
 
+import com.ayning.alexchain.util.DigestUtil;
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * SHA-256算法的Java实现
- *
+ * <p>
  * created by 10127 on 2018/2/28
  */
 public class Sha256 {
+
+    @Test
+    public void testSha256() {
+        String plain = "I like eating";
+        Assert.assertNotNull(DigestUtil.sha256(plain));
+    }
+
+
 }
